aa1ba3da98ce8b55770a5ccf805348a3d1d30e25
sofjagyy <sofjagy@yandex.ru>
2026-04-06 14:01:54 +0200

    logging

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/controller/ErrorHandler.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/controller/ErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.delivery.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,8 +12,11 @@ import java.util.Map;
 @RestControllerAdvice
 public class ErrorHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<Map<String, String>> handleApiException(ApiException e) {
+        log.warn("{} {}", e.getHttpStatus().value(), e.getMessage());
         return ResponseEntity.status(e.getHttpStatus())
                 .body(Map.of(
                         "httpStatus", e.getHttpStatus().toString(),
